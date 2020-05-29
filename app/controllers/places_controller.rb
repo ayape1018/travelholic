1ad8805a4b786893a7326f95d24b63e53a448d7a
ayape1018 <ayape1018ca@gmail.com>
2020-05-28 23:51:59 -0400

    add CRUD of place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,50 @@
 class PlacesController < ApplicationController
+  before_action :set_work, only: [:show, :edit, :update, :destroy]
+
   def index
+    @places = Place.all
   end
+
+  def show
+  end
+
+  def create
+    @place = Place.new(place_params)
+    if @place.save
+      redirect_to places_path
+    else
+      render 'new'
+    end
+  end
+
+  def new
+    @place = Place.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @place.update(place_params)
+      redirect_to place_path(@place)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @place.destroy
+    redirect_to places_path
+  end
+
+  private
+    def set_work
+      @place = Place.find(prams[:id])
+    end
+
+    def place_params
+      params[:place].permit(:name, :address, :telephone, :url, :image)
+    end
+
+
 end
